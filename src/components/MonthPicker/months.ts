5cb285ec5fc90ba22a2d13806d78e59b979c4018
Mauricio Bomfim <mauricio@raio.vc>
2025-10-03 16:17:28 -0300

    feat: initial commit

---
+++ src/components/MonthPicker/months.ts
@@ -0,0 +1,16 @@
+const months = [
+  'components:monthPicker.months.january',
+  'components:monthPicker.months.february',
+  'components:monthPicker.months.march',
+  'components:monthPicker.months.april',
+  'components:monthPicker.months.may',
+  'components:monthPicker.months.june',
+  'components:monthPicker.months.july',
+  'components:monthPicker.months.august',
+  'components:monthPicker.months.september',
+  'components:monthPicker.months.october',
+  'components:monthPicker.months.november',
+  'components:monthPicker.months.december',
+] as const;
+
+export default months;
